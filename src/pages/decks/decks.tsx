a1cea92fba294a08430de31d4fe5ace214c3c75f
Andrew Chernoff <whitedrew538@gmail.com>
2023-11-14 17:59:44 +0500

    optimistic and pessimistic update

--- src/pages/decks/decks.tsx
+++ src/pages/decks/decks.tsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom'
 
-import { Arrow } from '../../common/svg/sorting-arrow'
 import { formatDate } from '../../common/utils/time-transfering'
+import { Button } from '../../components/ui/button'
 import { Table } from '../../components/ui/table/table'
 import {
   useAddDeckMutation,
+  useDeleteDeckMutation,
   useGetDecksQuery,
-  useLazyGetDecksQuery,
 } from '../../services/decks/decks'
 
 import s from './decks.module.scss'
@@ -14,21 +14,22 @@ import s from './decks.module.scss'
 const Decks = () => {
   //const [filter, setFilter] = useState<'name-desc' | 'name-asc'>('name-asc')
 
-  const decks = useGetDecksQuery({ itemsPerPage: 25 /* , orderBy: filter */ })
+  const { data: decks } = useGetDecksQuery({ itemsPerPage: 25 /* , orderBy: filter */ })
   const [addDeck] = useAddDeckMutation()
-  const [getDecks2, decks2] = useLazyGetDecksQuery()
+  const [deleteDeck] = useDeleteDeckMutation()
 
   /*  const filterDirection = () =>
     filter === 'name-asc' ? setFilter('name-desc') : setFilter('name-asc')*/
 
-  console.log(decks2)
+  const onDeleteDeck = (id: string) => {
+    deleteDeck({ id })
+  }
 
   return (
     <div className={s.decks}>
       <Link to="/2">Go</Link>
 
-      <button onClick={() => addDeck('слегка выгорел)')}>add</button>
-      <button onClick={() => getDecks2()}>DECKS2</button>
+      <button onClick={() => addDeck({ name: '55556' })}>add</button>
 
       <Table.Root className={s.table}>
         <Table.Head>
@@ -45,13 +46,18 @@ const Decks = () => {
           </Table.Row>
         </Table.Head>
         <Table.Body>
-          {decks.data?.items.map((deck: any) => {
+          {decks?.items.map((deck: any) => {
             return (
               <Table.Row className={s.dataRow} key={deck.id}>
                 <Table.DataCell className={s.dataCell}>{deck.name}</Table.DataCell>
                 <Table.DataCell className={s.dataCell}>{deck.cardsCount}</Table.DataCell>
                 <Table.DataCell className={s.dataCell}>{formatDate(deck.updated)}</Table.DataCell>
-                <Table.DataCell className={s.dataCell}>{deck.author.name}</Table.DataCell>
+                <Table.DataCell className={s.dataCell}>
+                  {deck.author.name}
+                  <Button variant="purple" callBack={() => onDeleteDeck(deck.id)}>
+                    Delete
+                  </Button>
+                </Table.DataCell>
               </Table.Row>
             )
           })}
--- src/services/decks/decks.ts
+++ src/services/decks/decks.ts
@@ -1,5 +1,7 @@
 import { baseApi } from '../base-api'
 
+import { DeckResponse, DecksParams } from './types'
+
 const decksApi = baseApi.injectEndpoints({
   endpoints: builder => {
     return {
@@ -13,12 +15,54 @@ const decksApi = baseApi.injectEndpoints({
         },
         providesTags: ['Decks'],
       }),
-      addDeck: builder.mutation<any, string>({
-        query(name) {
+      addDeck: builder.mutation<any, { name: string }>({
+        /* query(name) {
           return {
             url: `v1/decks`,
             method: 'POST',
             body: { name },
+          }
+        }, */
+        query: data => ({
+          url: `v1/decks`,
+          method: 'POST',
+          body: data,
+        }),
+        async onQueryStarted(_, { dispatch, queryFulfilled }) {
+          try {
+            const res = await queryFulfilled
+
+            console.log(res)
+
+            /* const patchResult = */ dispatch(
+              decksApi.util.updateQueryData('getDecks', undefined, draft => {
+                // Object.assign(draft, updatedPost)
+                draft.items.push(res.data)
+              })
+            )
+          } catch (error) {
+            console.log(error)
+          }
+        },
+        invalidatesTags: ['Decks'],
+      }),
+      deleteDeck: builder.mutation<any, { id: string }>({
+        query: data => ({
+          url: `v1/decks/${data.id}`,
+          method: 'DELETE',
+        }),
+        async onQueryStarted({ id }, { dispatch, queryFulfilled }) {
+          const patchResult = dispatch(
+            decksApi.util.updateQueryData('getDecks', undefined, draft => {
+              /*  draft.items = */ draft.items.filter(el => el.id !== id)
+            })
+          )
+
+          try {
+            await queryFulfilled
+          } catch (error) {
+            patchResult.undo()
+            console.log(error)
           }
         },
         invalidatesTags: ['Decks'],
@@ -27,4 +71,4 @@ const decksApi = baseApi.injectEndpoints({
   },
 })
 
-export const { useGetDecksQuery, useLazyGetDecksQuery, useAddDeckMutation } = decksApi
+export const { useGetDecksQuery, useDeleteDeckMutation, useAddDeckMutation } = decksApi
