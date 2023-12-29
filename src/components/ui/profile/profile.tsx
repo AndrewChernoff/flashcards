2e48609203e5e44eedc4a3e12f55ffd599b0eea4
Andrew Chernoff <whitedrew538@gmail.com>
2023-12-30 00:26:42 +0500

    made not found component

--- src/components/ui/profile/profile.tsx
+++ src/components/ui/profile/profile.tsx
@@ -1,3 +1,5 @@
+import { Navigate } from 'react-router-dom'
+
 import { useAppSelector } from '../../../common/hooks/redux-hooks'
 import PersonalInfo from '../personal-info/personal-info'
 
@@ -5,7 +7,7 @@ const Profile = () => {
   const me = useAppSelector(state => state.auth.user)
 
   if (me === null) {
-    return
+    return <Navigate to="/*" />
   }
 
   return <PersonalInfo name={me?.name} email={me?.email} ava={me?.avatar} />
